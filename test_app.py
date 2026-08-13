e59dd1d182b41f4fefd9e530e8a3e60918666ddb
Tanish Anjum M.S <tanishanjum04@gmail.com>
2026-08-13 12:50:16 +0530

    small changes has be done

--- test_app.py
+++ test_app.py
@@ -5,7 +5,7 @@ def test_home():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
-    assert response.data == b"Hello from my automated CI/CD pipeline - v2!"
+    assert response.data == b"Hello from my automated CI/CD pipeline - v3!"
 
 
 def test_health():
