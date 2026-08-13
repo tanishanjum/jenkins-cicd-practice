7d943fcfd5c9ecbd67d3f462f698e25d259ff3bd
Tanish Anjum M.S <tanishanjum04@gmail.com>
2026-08-13 12:22:42 +0530

    make changes in test_app.py

--- test_app.py
+++ test_app.py
@@ -5,7 +5,7 @@ def test_home():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
-    assert response.data == b"Hello from my CI/CD practice application!"
+    assert response.data == b"Hello from my automated CI/CD pipeline!"
 
 
 def test_health():
